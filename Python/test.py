b9a1a15b97731cd0aa5b1d8362c1d0997021e688
fjuf97 <friedrich.foell@mail.de>
2022-12-22 09:28:43 +0100

    test files

--- Python/test.py
+++ Python/test.py
@@ -1,6 +1,9 @@
+#!C:\xampp\htdocs\Weichware\Python\python.exe
 from gtts import gTTS
 import playsound
 import os
+print("Content-Type: text/html\n\n")
+print("Hello world! Python works!")
 language = "de"
 tts = gTTS(text = "das ist ein kleiner test", lang = language, slow = False)
 tts.save('text.mp3')
